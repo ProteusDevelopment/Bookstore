db43f143eae2b1c5b3347600b9bd8802844d9f68
Artyom Chernykh <artcher2000@yandex.ru>
2022-02-15 21:20:04 +0300

    First commit.

---
+++ users/src/test/java/com/devcher/bookstore/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcher.bookstore.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
